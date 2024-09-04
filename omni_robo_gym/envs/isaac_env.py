60b6cda690d7a71f8b210dc5d5504b7a47ea2e12
Andrea Patrizi <andrepatri@hhcm-pilot-ws-2404.iit.local>
2024-09-04 17:22:07 +0200

    increased contact capacity

--- omni_robo_gym/envs/isaac_env.py
+++ omni_robo_gym/envs/isaac_env.py
@@ -222,11 +222,11 @@ class IsaacSimEnv(LRhcEnvBase):
         # isaac_opts["max_depenetration_velocity"] = 100.0
         # isaac_opts["solver_velocity_iteration_count"] = 1
         # GPU buffers settings
-        # isaac_opts["gpu_max_rigid_contact_count"] = 512 * 1024
-        # isaac_opts["gpu_max_rigid_patch_count"] = 80 * 1024
-        isaac_opts["gpu_found_lost_pairs_capacity"] = 4096
-        isaac_opts["gpu_found_lost_aggregate_pairs_capacity"] = 4096
-        # isaac_opts["gpu_total_aggregate_pairs_capacity"] = 1024
+        isaac_opts["gpu_max_rigid_contact_count"] = 512 * 1024
+        isaac_opts["gpu_max_rigid_patch_count"] = 80 * 1024
+        isaac_opts["gpu_found_lost_pairs_capacity"] = 8192
+        isaac_opts["gpu_found_lost_aggregate_pairs_capacity"] = 8192
+        isaac_opts["gpu_total_aggregate_pairs_capacity"] = 8192
         # isaac_opts["gpu_max_soft_body_contacts"] = 1024 * 1024
         # isaac_opts["gpu_max_particle_contacts"] = 1024 * 1024
         # isaac_opts["gpu_heap_capacity"] = 64 * 1024 * 1024
