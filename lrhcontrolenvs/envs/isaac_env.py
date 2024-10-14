9ba5c2ad2ba292fb099fac839b2183ab8b9c4a1a
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-10-14 18:28:26 +0200

    increased gpu paris count

--- lrhcontrolenvs/envs/isaac_env.py
+++ lrhcontrolenvs/envs/isaac_env.py
@@ -230,9 +230,9 @@ class IsaacSimEnv(LRhcEnvBase):
         # GPU buffers settings
         isaac_opts["gpu_max_rigid_contact_count"] = 512 * 1024
         isaac_opts["gpu_max_rigid_patch_count"] = 80 * 1024
-        isaac_opts["gpu_found_lost_pairs_capacity"] = 8192
-        isaac_opts["gpu_found_lost_aggregate_pairs_capacity"] = 8192
-        isaac_opts["gpu_total_aggregate_pairs_capacity"] = 8192
+        isaac_opts["gpu_found_lost_pairs_capacity"] = 51200
+        isaac_opts["gpu_found_lost_aggregate_pairs_capacity"] = 51200
+        isaac_opts["gpu_total_aggregate_pairs_capacity"] = 51200
         # isaac_opts["gpu_max_soft_body_contacts"] = 1024 * 1024
         # isaac_opts["gpu_max_particle_contacts"] = 1024 * 1024
         # isaac_opts["gpu_heap_capacity"] = 64 * 1024 * 1024
