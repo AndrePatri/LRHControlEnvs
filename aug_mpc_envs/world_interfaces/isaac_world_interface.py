8bb8f2e41771d73766d692fc9c1fcb0d189136f4
Andrea.Patrizi@iit.it <apatrizi@fe01.priv.franklin.lan>
2026-01-13 12:02:27 +0100

    default to smaller steps and more steps

--- aug_mpc_envs/world_interfaces/isaac_world_interface.py
+++ aug_mpc_envs/world_interfaces/isaac_world_interface.py
@@ -309,7 +309,7 @@ class IsaacSimEnv(AugMPCWorldInterfaceBase):
         isaac_opts["dh_ground"]=0.03
         isaac_opts["step_height_lb"]=0.08
         isaac_opts["step_height_ub"]=0.15
-        isaac_opts["step_width_lb"]=1.0
+        isaac_opts["step_width_lb"]=0.6
         isaac_opts["step_width_ub"]= 1.5
         isaac_opts["contact_prims"] = []
         isaac_opts["sensor_radii"] = 0.1
@@ -664,7 +664,7 @@ class IsaacSimEnv(AugMPCWorldInterfaceBase):
                     static_friction=self._env_opts["static_friction"], 
                     dynamic_friction=self._env_opts["dynamic_friction"], 
                     restitution=self._env_opts["restitution"],
-                    n_steps=22,
+                    n_steps=25,
                     area_factor=0.7,
                     random_n_steps=False
                     )
